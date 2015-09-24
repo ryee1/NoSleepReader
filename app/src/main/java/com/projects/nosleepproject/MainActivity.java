ca24a446f329d63c6c4719b97be72f9b87368b16
Richard <rdyyc99@gmail.com>
2015-09-23 18:50:07 -0700

    Set up navigation drawer backbone

--- app/src/main/java/com/projects/nosleepproject/MainActivity.java
+++ app/src/main/java/com/projects/nosleepproject/MainActivity.java
@@ -3,7 +3,11 @@ package com.projects.nosleepproject;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private ListView mListView;
     private ListViewAdapter mAdapter;
     private List<ContentValues> mValuesArray;
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
 
     private boolean firstRun;
     private static String MFRAG_TAG = "mfrag";
@@ -35,6 +41,30 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         }
 
         mListView = (ListView) findViewById(R.id.listView);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_drawer);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView nView = (NavigationView) findViewById(R.id.nvView);
+        setupDrawerContent(nView);
+    }
+
+    public void setupDrawerContent(NavigationView nView){
+        nView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                switch(id){
+                    
+                }
+
+                return true;
+            }
+        });
     }
 
     @Override
@@ -76,12 +106,20 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.action_settings:
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
     }
 
     @Override
